2f335d881c248b3e5a41565450a888fcf24dcd6a
lesego.lelaka <lesego.lelaka@boxfusion.io>
2021-11-15 12:22:59 +0200

    Pulling current

---
+++ src/Boxfusion.Health.His.Administration/Services/Reports/IReportAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.His.Administration.Services.Reports
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IReportAppService : IApplicationService
+    {
+
+    }
+}
