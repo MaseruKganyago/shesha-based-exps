f8735b4b1c48fd79553c0ddb0b86cfde7a7f74ff
Moses <Matimba.moses@boxfusion.io>
2021-11-29 20:51:51 +0200

    null error fix

--- src/Boxfusion.Health.His.Admissions/Services/Admissions/AdmissionsAppService.cs
+++ src/Boxfusion.Health.His.Admissions/Services/Admissions/AdmissionsAppService.cs
@@ -129,7 +129,7 @@ namespace Boxfusion.Health.His.Admissions.Services.Admissions
             }
             else
             {
-                if (wardAdmission.InternalTransferOriginalWard.Id == null)
+                if (wardAdmission?.InternalTransferOriginalWard?.Id == null)
                     throw new UserFriendlyException("The Previous ward record was not found");
 
                 var originalWard = await wardAdmissionService.GetAsync(wardAdmission.InternalTransferOriginalWard.Id);
