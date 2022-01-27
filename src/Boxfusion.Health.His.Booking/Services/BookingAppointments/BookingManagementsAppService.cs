f429ff0511c2d80abe991edb26fd81c575f1c260
Paul Neo Nkala <NeoN@boxfusion.co.za>
2022-01-27 22:23:05 +0200

    incomplete

---
+++ src/Boxfusion.Health.His.Booking/Services/BookingAppointments/BookingManagementsAppService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Authorization;
+using Boxfusion.Health.HealthCommon.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.His.Bookings.Services.BookingAppointments
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AbpAuthorize]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/His/[controller]")]
+    public class BookingManagementsAppService : CdmAppServiceBase, IBookingManagementsAppService
+    {
+
+    }
+}
---
+++ src/Boxfusion.Health.His.Booking/Services/BookingAppointments/IBookingManagementsAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.His.Bookings.Services.BookingAppointments
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBookingManagementsAppService: IApplicationService
+    {
+
+    }
+}
