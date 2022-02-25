c6326061f6e0988f4e6e1d674c8ee703a3f35763
Moses <Hlongwanimm@gmail.com>
2022-02-25 13:32:58 +0200

    tests fix

--- test/Boxfusion.Health.His.Tests/BookingSlots/BookingSlots_Test.cs
+++ test/Boxfusion.Health.His.Tests/BookingSlots/BookingSlots_Test.cs
@@ -14,91 +14,89 @@ namespace Boxfusion.Health.His.Tests.BookingSlots
     public class BookingSlots_Test : SheshaNhTestBase<HisTestModule>
     {
         private readonly IGenerateBookingSlotsHelper _generateBookingSlotsHelper;
-        //public List<CdmSchedule> Schedules { get; set; }
-        //public List<ScheduleAvailabilityForBooking> ScheduleAvailabilities { get; set; }
+        public List<CdmSchedule> Schedules { get; set; }
+        public List<ScheduleAvailabilityForBooking> ScheduleAvailabilities { get; set; }
         public BookingSlots_Test()
-        {            
+        {
             _generateBookingSlotsHelper = Resolve<IGenerateBookingSlotsHelper>();
-            //Schedules = new List<CdmSchedule>();
-            //ScheduleAvailabilities = new List<ScheduleAvailabilityForBooking>();
+            Schedules = new List<CdmSchedule>();
+            ScheduleAvailabilities = new List<ScheduleAvailabilityForBooking>();
         }
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        //[Fact]
-        //public async Task Should_Getschedules()
-        //{
-        //    LoginAsHost("admin");
-        //    //Schedules = await _generateBookingSlotsHelper.GetSchedules();
+        [Fact]
+        public async Task Should_Getschedules()
+        {
+            LoginAsHost("admin");
+            Schedules = await _generateBookingSlotsHelper.GetSchedules();
 
-        //    //Assert.NotNull(Schedules);
-
-           
-        //}
+            Assert.NotNull(Schedules);
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        //[Fact]
-        //public async Task Should_GetscheduleAvailability()
-        //{
-        //    LoginAsHost("admin");
-        //    //foreach (var schedule in Schedules)
-        //    //{
-        //    //    ScheduleAvailabilities.AddRange(await _generateBookingSlotsHelper.GetScheduleAvailability(schedule));
-        //    //}
+        [Fact]
+        public async Task Should_GetscheduleAvailability()
+        {
+            LoginAsHost("admin");
+            foreach (var schedule in Schedules)
+            {
+                ScheduleAvailabilities.AddRange(await _generateBookingSlotsHelper.GetScheduleAvailability(schedule));
+            }
 
-        //    //Assert.NotNull(ScheduleAvailabilities);
-        //}
+            Assert.NotNull(ScheduleAvailabilities);
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        //[Fact]
-        //public async Task Should_Have_ApplicableDays()
-        //{
-        //    //foreach(var scheduleAvailability in ScheduleAvailabilities)
-        //    //{
-        //    //    Assert.NotNull(scheduleAvailability.ApplicableDays);
-        //    //}
-        //} 
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <returns></returns>
-        //[Fact]
-        //public async Task ApplicableDays_Should_Not_Fall_On_Public_Holiday()
-        //{
-        //    //foreach(var scheduleAvailability in ScheduleAvailabilities)
-        //    //{
-        //    //    Assert.True(scheduleAvailability.ApplicableDays != RefListDaysOfWeek.pub);
-        //    //}
-        //}
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <returns></returns>
-        //[Fact]
-        //public async Task LastGeneratedSlotDateShouldbeBiggerThanValidFromDate()
-        //{
-        //    //foreach (var scheduleAvailability in ScheduleAvailabilities)
-        //    //{
-        //    //    Assert.True(scheduleAvailability.LastGeneratedSlotDate > scheduleAvailability.ValidFromDate);
-        //    //}
-        //}
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <returns></returns>
-        //[Fact]
-        //public async Task LastGeneratedSlotDateShouldbeSmallerThanValidFromDate()
-        //{
-        //    //foreach (var scheduleAvailability in ScheduleAvailabilities)
-        //    //{
-        //    //    Assert.True(scheduleAvailability.LastGeneratedSlotDate > scheduleAvailability.ValidToDate);
-        //    //}
-        //}
+        [Fact]
+        public async Task Should_Have_ApplicableDays()
+        {
+            foreach (var scheduleAvailability in ScheduleAvailabilities)
+            {
+                Assert.NotNull(scheduleAvailability.ApplicableDays);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ApplicableDays_Should_Not_Fall_On_Public_Holiday()
+        {
+            foreach (var scheduleAvailability in ScheduleAvailabilities)
+            {
+                Assert.True(scheduleAvailability.ApplicableDays != RefListDaysOfWeek.pub);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task LastGeneratedSlotDateShouldbeBiggerThanValidFromDate()
+        {
+            foreach (var scheduleAvailability in ScheduleAvailabilities)
+            {
+                Assert.True(scheduleAvailability.LastGeneratedSlotDate > scheduleAvailability.ValidFromDate);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task LastGeneratedSlotDateShouldbeSmallerThanValidFromDate()
+        {
+            foreach (var scheduleAvailability in ScheduleAvailabilities)
+            {
+                Assert.True(scheduleAvailability.LastGeneratedSlotDate > scheduleAvailability.ValidToDate);
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
