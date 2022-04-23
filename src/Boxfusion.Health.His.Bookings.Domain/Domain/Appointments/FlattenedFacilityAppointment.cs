0b5135fd1ec0bb5e7202765c5ae1e19a928fbaf6
AlexS <alexs@boxfusion.co.za>
2022-04-23 13:16:38 +0300

    Update to ABP 6.6.2

--- src/Boxfusion.Health.His.Bookings.Domain/Domain/Appointments/FlattenedFacilityAppointment.cs
+++ src/Boxfusion.Health.His.Bookings.Domain/Domain/Appointments/FlattenedFacilityAppointment.cs
@@ -1,6 +1,5 @@
 ﻿using Abp.Application.Services.Dto;
 using Abp.Domain.Entities;
-using Boxfusion.Health.Cdm.Schedules;
 using Boxfusion.Health.HealthCommon.Core.Domain.Cdm.Enum;
 using Boxfusion.Health.HealthCommon.Core.Domain.Fhir.Enum;
 using Shesha.AutoMapper.Dto;
@@ -9,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using Boxfusion.Health.Cdm.Schedules;
 
 namespace Boxfusion.Health.His.Bookings.Domain
 {
