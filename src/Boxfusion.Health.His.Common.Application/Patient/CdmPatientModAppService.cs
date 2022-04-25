8ba2db8a6ac7211f60ae4edb37450019a235640f
Jabulani Nkomo <Jabulani.Nkomo@boxfusion.io>
2022-04-25 13:33:11 +0200

    Added endpoint for getting CdmPatient by IdNumber

---
+++ src/Boxfusion.Health.His.Common.Application/Patient/CdmPatientModAppService.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Authorization;
+using Abp.Domain.Repositories;
+using Boxfusion.Health.Cdm.Patients;
+using Boxfusion.Health.HealthCommon.Core.Dtos.Cdm;
+using Boxfusion.Health.HealthCommon.Core.Services.Patients.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Common.Patient
+{
+    public class CdmPatientModAppService : HisAppServiceBase
+    {
+        private readonly IPatientCrudHelper<CdmPatient, CdmPatient> _patientCrudHelper;
+
+
+        [HttpGet, Route("/api/services/Cdm/CdmPatient/GetByIdentityNumber/{identityNumber}")]
+        public async Task<CdmPatient> GetCdmPatientByIdNumberAsync(string identityNumber)
+        {
+            return await _patientCrudHelper.GetByIdNumberAsync(identityNumber);
+        }
+    }
+}
