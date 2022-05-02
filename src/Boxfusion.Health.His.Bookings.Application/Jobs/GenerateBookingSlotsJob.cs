e26c335bf6d4ce774d6f915bf97e16ed703eacf4
ivan <ivan@boxfusion.co.za>
2022-05-02 14:56:16 +0300

    moved favicon.ico to the correct folder fixed some failing jobs updated submodules

--- src/Boxfusion.Health.His.Bookings.Application/Jobs/GenerateBookingSlotsJob.cs
+++ src/Boxfusion.Health.His.Bookings.Application/Jobs/GenerateBookingSlotsJob.cs
@@ -1,20 +1,8 @@
 ﻿using Abp.Dependency;
-using Abp.Domain.Repositories;
-using Boxfusion.Health.HealthCommon.Core.Domain.Cdm;
-using Boxfusion.Health.HealthCommon.Core.Domain.Cdm.Enum;
-using Boxfusion.Health.HealthCommon.Core.Domain.Fhir;
-using Boxfusion.Health.HealthCommon.Core.Domain.Fhir.Enum;
-using Boxfusion.Health.HealthCommon.Core.Helpers;
 using Boxfusion.Health.His.Bookings.Domain;
-using Castle.Core.Logging;
-using NHibernate.Linq;
 using Shesha.Scheduler;
 using Shesha.Scheduler.Attributes;
 using Shesha.Scheduler.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -46,7 +34,11 @@ namespace Boxfusion.Health.His.Bookings.Jobs
         /// <returns></returns>
         public override async Task DoExecuteAsync(CancellationToken cancellationToken)
         {
-            await _bookingSlotsGenerator.GenerateBookingSlotsForAllSchedulesAsync();
+            using (var uow = UnitOfWorkManager.Begin()) 
+            {
+                await _bookingSlotsGenerator.GenerateBookingSlotsForAllSchedulesAsync();
+                await uow.CompleteAsync();
+            }                
         }
     }
 }
