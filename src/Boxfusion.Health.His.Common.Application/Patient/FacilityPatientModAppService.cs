d2d8b426269b7864158fb3339569fbd4dbcfa80d
Jabulani Nkomo <Jabulani.Nkomo@boxfusion.io>
2022-05-04 14:37:10 +0200

    Bug fixes

--- src/Boxfusion.Health.His.Common.Application/Patient/FacilityPatientModAppService.cs
+++ src/Boxfusion.Health.His.Common.Application/Patient/FacilityPatientModAppService.cs
@@ -19,25 +19,36 @@ namespace Boxfusion.Health.His.Common.Patient
         [HttpGet, Route("/api/services/Common/FacilityPatient/GetByIdentityNumber/{identityNumber}")]
         public async Task<FacilityPatientDto> GetPatientByIdNumberAsync(string identityNumber)
         {
-           var patients =  await _patientRepository.GetAllListAsync(facilityPatient => facilityPatient.IdentityNumber == identityNumber);
-
             var response = new FacilityPatientDto();
-            
-            if (patients.Count == 0)
+
+            try
+            {
+                var patients = await _patientRepository.GetAllListAsync(facilityPatient => facilityPatient.IdentityNumber == identityNumber);
+
+
+                if (patients.Count == 0)
+                {
+                    response.Code = "OK";
+                    response.Message = "That ID is unique";
+                    return response;
+                }
+                else
+                {
+                    var patientNames = "";
+                    patients.ForEach(patient => patientNames += patient.FullNameWithTitle + "\n");
+
+                    response.Code = "DUPLICATE";
+                    response.Message = "That ID already belongs to : \n " + patientNames;
+                    return response;
+                }
+            }
+            catch (Exception ex)
             {
                 response.Code = "OK";
-                response.Message = "That ID is unique";
+                response.Message = "That ID wasn't found";
                 return response;
             }
-            else
-            {
-                var patientNames = "";
-                patients.ForEach(patient => patientNames += patient.FullNameWithTitle + "\n");
-
-                response.Code = "DUPLICATE";
-                response.Message = "That ID already belongs to : \n " + patientNames;
-                return response;
-            }
+          
                
            
         }
