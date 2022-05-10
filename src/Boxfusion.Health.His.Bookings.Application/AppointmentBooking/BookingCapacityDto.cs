f0815ec0a626903cb2720bb1dfa8be7c6e2b1550
Ian Houvet <ian.houvet@boxfusion.io>
2022-05-10 18:05:15 +0200

    Added: AppointmentBookingAppService.GetAvailableDayBookingCapacityAsync

---
+++ src/Boxfusion.Health.His.Bookings.Application/AppointmentBooking/BookingCapacityDto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Bookings.AppointmentBooking
+{
+    public class BookingCapacityDto
+    {
+        public virtual Guid? ScheduleId { get; set; }
+
+        /// <summary>
+        /// Date/Time that the slot is to begin.
+        /// </summary>
+        public virtual DateTime? StartDateTime { get; set; }
+
+        /// <summary>
+        /// Date/Time that the slot is to conclude.
+        /// </summary>
+        public virtual DateTime? EndDateTime { get; set; }
+
+        /// <summary>
+        /// The number of Appointments allowed to be booked against this Slot.
+        /// </summary>
+        public virtual int? Capacity { get; set; }
+
+        /// <summary>
+        /// The number of 'Overflow' Appointments allowed to be booked against this Slot.
+        /// </summary>
+        public virtual int? OverflowCapacity { get; set; }
+
+        /// <summary>
+        /// The total number of valid bookings which have been made against this slot. (Calculated DB Column)
+        /// </summary>
+        public virtual int NumValidAppointments { get; set; }
+
+        /// <summary>
+        /// The Regular capacity still available to be booked i.e. excluding Overflow capacity.
+        /// </summary>
+        public virtual int RemainingRegularCapacity
+        {
+            get
+            {
+                return (NumValidAppointments >= Capacity) ? 0 : (Capacity ?? 0) - NumValidAppointments;
+            }
+        }
+
+        /// <summary>
+        /// Total capacity still available to be booked inclduing both Regular and Overflow
+        /// </summary>
+        public virtual int RemainingCapacity
+        {
+            get
+            {
+                return (Capacity ?? 0) + (OverflowCapacity ?? 0) - NumValidAppointments;
+            }
+        }
+    }
+
+}
