1c867f9f005c9bac700ac9f93b0843cbd10ccad5
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-11 08:54:29 +0200

    Re-added CoCoverage

---
+++ src/Boxfusion.Health.His.Common.Domain/Domain/CoCoverage.cs
@@ -0,0 +1,26 @@
+﻿using Boxfusion.Health.Cdm.Domain.Domain.Fhir;
+using Shesha.Domain;
+using Shesha.Domain.Attributes;
+using Shesha.Enterprise.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Common.Domain.Domain
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Entity(TypeShortAlias = "His.CoCoverage")]
+	[Table("Fhir_Coverages")]
+	public class CoCoverage : Coverage
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public virtual BankAccount BankAccount { get; set; }
+	}
+}
