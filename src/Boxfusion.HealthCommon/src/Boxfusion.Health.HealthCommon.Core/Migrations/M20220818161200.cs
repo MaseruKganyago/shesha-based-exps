7cb64d72e712cbdbdb7dc5093d5af850e9c956b7
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-18 16:13:50 +0200

    Minor fix

---
+++ src/Boxfusion.HealthCommon/src/Boxfusion.Health.HealthCommon.Core/Migrations/M20220818161200.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.HealthCommon.Core.Migrations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Migration(20220818161200)]
+    public class M20220818161200 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Alter.Table("Core_Organisations")
+                .AddColumn("Fhir_TypeLkp").AsInt64().Nullable();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
