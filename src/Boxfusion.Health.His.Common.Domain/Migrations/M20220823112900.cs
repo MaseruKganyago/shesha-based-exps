521cc79307ec438ab27bb9370da8823b6c02d53b
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-23 13:51:33 +0200

    Updated wardAdmission

---
+++ src/Boxfusion.Health.His.Common.Domain/Migrations/M20220823112900.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.His.Domain.Migrations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Migration(20220823112900)]
+    public class M20220823112900 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Alter.Table("Fhir_Encounters")
+                 .AddForeignKeyColumn("His_BedId", "His_Beds")
+                 .AddForeignKeyColumn("His_RoomId", "His_Rooms");
+		}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
