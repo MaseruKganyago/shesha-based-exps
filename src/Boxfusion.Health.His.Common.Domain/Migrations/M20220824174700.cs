5e65d7ac962c9582e961e520ccaaddee5c0829d8
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-24 19:33:06 +0200

    Updated domains

---
+++ src/Boxfusion.Health.His.Common.Domain/Migrations/M20220824174700.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.His.Domain.Migrations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Migration(20220824174700)]
+    public class M20220824174700 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Delete.ForeignKey("FK_Fhir_Coverages_ClassId_Fhir_Classes_Id").OnTable("Fhir_Coverages");
+            Delete.Index("IX_Fhir_Coverages_ClassId").OnTable("Fhir_Coverages");
+            Delete.Column("ClassId").FromTable("Fhir_Coverages");
+
+            Alter.Table("Fhir_Coverages")
+                .AddColumn("Class").AsString().Nullable();
+		}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
