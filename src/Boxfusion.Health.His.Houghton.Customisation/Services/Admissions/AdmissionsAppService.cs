98e58c6782a01b1d41cccfe78eeccf287aee0226
Qoane Seitlheko <qoane.seitlheko@boxfusion.io>
2022-08-24 14:32:56 +0200

    Added custom API for admitting

---
+++ src/Boxfusion.Health.His.Houghton.Customisation/Services/Admissions/AdmissionsAppService.cs
@@ -0,0 +1,75 @@
+﻿using Abp.Authorization;
+using Abp.Domain.Repositories;
+using Boxfusion.Health.HealthCommon.Core.Domain.BackBoneElements.Enum;
+using Boxfusion.Health.HealthCommon.Core.Domain.BackBoneElements.Fhir;
+using Boxfusion.Health.HealthCommon.Core.Domain.Fhir;
+using Boxfusion.Health.His.Admissions.Domain.Domain.Admissions.Dtos;
+using Boxfusion.Health.His.Common.Admissions;
+using Boxfusion.Health.His.Common.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Shesha;
+using Shesha.Domain;
+using Shesha.DynamicEntities.Dtos;
+using Shesha.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Houghton.Customisation.Services.Admissions
+{
+    [AbpAuthorize]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/Hough/[controller]")]
+    public class AdmissionsAppService : SheshaAppServiceBase
+    {
+        private readonly IRepository<WardAdmission, Guid> _wardAdmissionRepositiory;
+        private readonly IRepository<Condition, Guid> _conditionRepository;
+        private readonly IRepository<Diagnosis, Guid> _diagnosisRepository;
+        private readonly IRepository<Note, Guid> _noteRepository;
+
+        public AdmissionsAppService(IRepository<WardAdmission, Guid> wardAdmissionRepositiory, IRepository<Condition, Guid> conditionRepository, IRepository<Diagnosis, Guid> diagnosisRepository, IRepository<Note, Guid> noteRepository)
+        {
+            _wardAdmissionRepositiory = wardAdmissionRepositiory;
+            _conditionRepository = conditionRepository;
+            _diagnosisRepository = diagnosisRepository;
+            _noteRepository = noteRepository;
+
+        }
+
+        public async Task<DynamicDto<WardAdmission, Guid>> AdmitPatientAsync(WardAdmissionsDto input)
+        {
+            var wardAdmissionEntity = await _wardAdmissionRepositiory.InsertAsync(ObjectMapper.Map<WardAdmission>(input));
+
+            input.Conditions.ForEach(async condition => { await createDiagnosis(condition, wardAdmissionEntity); });
+
+            var note = new Note()
+            {
+                NoteText = input.AdmissionNotes,
+                OwnerId = wardAdmissionEntity.Id.ToString(),
+                OwnerType = wardAdmissionEntity.GetTypeShortAlias(),
+                Category = (int)RefListHisNoteType.admission
+
+
+            };
+            await _noteRepository.InsertAsync(note);
+            return await MapToDynamicDtoAsync<WardAdmission, Guid>(wardAdmissionEntity);
+        }
+
+        private async Task createDiagnosis(Guid conditionId, WardAdmission wardAdmissionEntity)
+        {
+            var condition = _conditionRepository.Get(conditionId);
+
+            var diagnosis = new Diagnosis()
+            {
+                Condition = condition,
+                OwnerId = wardAdmissionEntity.Id.ToString(),
+                OwnerType = wardAdmissionEntity.GetTypeShortAlias(),
+                Use = (int?)RefListEncounterDiagnosisRoles.AD
+            };
+            
+            await _diagnosisRepository.InsertAsync(diagnosis);
+        }
+    }
+}
---
+++ src/Boxfusion.Health.His.Houghton.Customisation/Services/Admissions/WardAdmissionsDto.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services.Dto;
+using Shesha.AutoMapper.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Houghton.Customisation.Services.Admissions
+{
+    public class WardAdmissionsDto : EntityDto<Guid>
+    {
+        public Guid Patient { get; set; }
+
+        public EntityWithDisplayNameDto<Guid> Ward { get; set; }
+
+        public EntityWithDisplayNameDto<Guid> Room { get; set; }
+
+        public EntityWithDisplayNameDto<Guid> Bed { get; set; }
+
+        public DateTime AdmissionDate { get; set; }
+
+        public ReferenceListItemValueDto AdmissionType { get; set; }
+
+        public  List<Guid> Conditions { get; set; }
+
+        public string AdmissionNotes { get; set; }
+    }
+}
