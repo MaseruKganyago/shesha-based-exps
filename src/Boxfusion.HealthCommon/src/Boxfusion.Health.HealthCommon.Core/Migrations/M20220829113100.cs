9897df4ca5bb8adad3b4512ed2f14e11f85e7795
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-29 11:50:44 +0200

    Added Fhir_PatientContacts table

---
+++ src/Boxfusion.HealthCommon/src/Boxfusion.Health.HealthCommon.Core/Migrations/M20220829113100.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.HealthCommon.Core.Migrations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Migration(20220829113100)]
+    public class M20220829113100 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table("Fhir_PatientContacts")
+                .WithIdAsGuid()
+                .WithFullAuditColumns()
+                .WithColumn("FirstName").AsString().Nullable()
+                .WithColumn("LastName").AsString().Nullable()
+                .WithColumn("RelationshipTypeLkp").AsInt64().Nullable()
+                .WithColumn("MobileNumber").AsString().Nullable()
+                .WithColumn("AlternativeNumber").AsString().Nullable()
+                .WithForeignKeyColumn("AddressId", "Core_Addresses");              
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+
