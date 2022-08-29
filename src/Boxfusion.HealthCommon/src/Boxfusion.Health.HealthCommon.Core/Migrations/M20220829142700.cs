d755fb0e7aab357d79cd0699c357ed6c533e8705
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-08-29 14:51:07 +0200

    Minor fix

---
+++ src/Boxfusion.HealthCommon/src/Boxfusion.Health.HealthCommon.Core/Migrations/M20220829142700.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxfusion.Health.HealthCommon.Core.Migrations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Migration(20220829142700)]
+    public class M20220829142700 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Delete.Table("Fhir_PatientContact");    
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+
