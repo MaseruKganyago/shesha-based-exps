f1e76c1fc5cba5b30c2784e42b4d83aad6ef812b
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-09-12 13:11:55 +0200

    ChargeItemManager_Test

---
+++ test/Boxfusion.Health.His.Common.Domain.Tests/HisCommonDomainTestBase.cs
@@ -0,0 +1,42 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Boxfusion.Health.His.Common.Patients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Health.His.Common.Tests
+{
+	public class HisCommonDomainTestBase: SheshaNhTestBase
+	{
+		private readonly IRepository<HisPatient, Guid> _patientRepository;
+		private readonly IUnitOfWorkManager _uowManager;
+
+		public HisCommonDomainTestBase()
+		{
+			_patientRepository = Resolve<IRepository<HisPatient, Guid>>();
+			_uowManager = Resolve<IUnitOfWorkManager>();
+		}
+
+		protected async Task<HisPatient> CreateTestData_NewPatient(string name)
+		{
+			HisPatient patient;
+
+			using (var uow = _uowManager.Begin())
+			{
+				patient = new HisPatient()
+				{
+					FirstName = name,
+					LastName = name
+				};
+				patient = await _patientRepository.InsertAsync(patient);
+
+				uow.Complete();
+			}
+
+			return patient;
+		}
+	}
+}
