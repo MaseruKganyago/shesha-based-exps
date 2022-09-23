46626c1b30996cb926b16ad2f947063825315d73
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-09-23 11:08:12 +0200

    Submodule update

--- Boxfusion.Health.His.Houghton.Customisations.Tests/WardAdmissions/WardAdmissionsAppService_Test.cs
+++ Boxfusion.Health.His.Houghton.Customisations.Tests/WardAdmissions/WardAdmissionsAppService_Test.cs
@@ -143,7 +143,7 @@ namespace Boxfusion.Health.His.Admissions.Application.Tests.WardAdmissions
 					Id = admissionData.Id,
 					DischargeDate = DateTime.Now,
 					DischargeNotes = "UnitTest Note",
-					Physician = "Dave Kate" + " :Test2"
+					//Physician = new 
 				};
 				#endregion
 
@@ -158,7 +158,7 @@ namespace Boxfusion.Health.His.Admissions.Application.Tests.WardAdmissions
 
 				//Verify wardAdmission was discharged
 				admission.AdmissionStatus.ShouldBe(RefListAdmissionStatuses.separated);
-				admission.EndDateTime?.ToString("MM/dd/yyyy HH:mm").ShouldBe(dischargeInput.DischargeDate.ToString("MM/dd/yyyy HH:mm"));
+				admission.EndDateTime?.ToString("MM/dd/yyyy HH:mm").ShouldBe(dischargeInput.DischargeDate.Value.ToString("MM/dd/yyyy HH:mm"));
 
 				//Verify discharge note was created
 				var note = await _noteRepository.FirstOrDefaultAsync(a => a.OwnerId == admission.Id.ToString());
