0e0908d56fec7d0f5feb585a75f84de6bd1d13e0
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-09-26 16:52:34 +0200

    Mapping fix on RegisterPatient api

--- src/Boxfusion.Health.His.Admissions/Services/PatientRegistrations/PatientRegistrationMapProfile.cs
+++ src/Boxfusion.Health.His.Admissions/Services/PatientRegistrations/PatientRegistrationMapProfile.cs
@@ -1,4 +1,5 @@
-﻿using Boxfusion.Health.His.Common.Patients;
+﻿using Boxfusion.Health.HealthCommon.Core.Helpers;
+using Boxfusion.Health.His.Common.Patients;
 using Shesha.AutoMapper;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,7 @@ namespace Boxfusion.Health.His.Admissions.PatientRegistrations
 				.ForMember(a => a.EmailAddress1, opt => opt.MapFrom(b => b.EmailAddress))
 				.ForMember(a => a.WorkTelephone, opt => opt.MapFrom(b => b.WorkTelephoneNo))
 				.ForMember(a => a.WorkAddress, opt => opt.Ignore())
+				.ForMember(a => a.CommunicationLanguage, opt => opt.MapFrom(b => UtilityHelper.SetMultiValueReferenceList(b.CommunicationLanguage)))
 				.MapReferenceListValuesFromDto();
 		}
 	}
