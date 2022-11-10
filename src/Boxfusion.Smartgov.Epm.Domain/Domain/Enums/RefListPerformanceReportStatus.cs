6ae8907324179fa6b0faa04bc5e1001b94afccf2
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-11-10 11:32:58 +0200

    updated Epm_PerformanceReports table

--- src/Boxfusion.Smartgov.Epm.Domain/Domain/Enums/RefListPerformanceReportStatus.cs
+++ src/Boxfusion.Smartgov.Epm.Domain/Domain/Enums/RefListPerformanceReportStatus.cs
@@ -8,28 +8,17 @@ using System.Threading.Tasks;
 
 namespace Boxfusion.Smartgov.Epm.Domain.Enums
 {
-    /// <summary>
-    /// 
-    /// </summary>
     [ReferenceList("Epm", "PerformanceReportStatus")]
     public enum RefListPerformanceReportStatus: long
     {
-        /// <summary>
-        /// 
-        /// </summary>
+
         [Description("Report is still not published and is still at Planning stage where reporting hierarchy, " +
 			"targets etc... are still being finalised i.e. should only be visible to Admins")]
         Planning = 10,
 
-        /// <summary>
-        /// 
-        /// </summary>
         [Description("We are within the period covered by the report and progress reporting is still in progress.")]
         ReportingInProgress = 20,
 
-        /// <summary>
-        /// 
-        /// </summary>
         [Description("The all progress reporting has been completed and report values have been vetted and finalised.")]
         Finalised = 30
     }
---
+++ src/Boxfusion.Smartgov.Epm.Domain/Migrations/M20221110101200.cs
@@ -0,0 +1,32 @@
+﻿
+using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Smartgov.Epm.Migrations
+{
+	[Migration(20221110101200)]
+	public class M20221110101200 : Migration
+	{
+		public override void Up()
+		{
+			#region Deleting ForeignKey TemplateId from Epm_PerformanceReports
+			Delete.ForeignKey("FK_Epm_PerformanceReports_TemplateId_Epm_PerformanceReports_Id").OnTable("Epm_PerformanceReports");
+			Delete.Index("IX_Epm_PerformanceReports_TemplateId").OnTable("Epm_PerformanceReports");
+			Delete.Column("TemplateId").FromTable("Epm_PerformanceReports");
+			#endregion
+
+			Alter.Table("Epm_PerformanceReports")
+				.AddForeignKeyColumn("TemplateId", "Epm_PerformanceReportTemplates");
+		}
+
+		public override void Down()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
