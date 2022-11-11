aec33067a3a8310326f479050f91fdb0c0a27995
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2022-11-11 15:25:14 +0200

    updated Epm_ComponentTypes table

---
+++ src/Boxfusion.Smartgov.Epm.Domain/Migrations/M20221111150200.cs
@@ -0,0 +1,25 @@
+﻿
+using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.Smartgov.Epm.Migrations
+{
+	[Migration(20221111150200)]
+	public class M20221111150200 : Migration
+	{
+		public override void Up()
+		{
+			Alter.Column("ProgressDetailsSubForm").OnTable("Epm_ComponentTypes").AsString().Nullable();
+		}
+
+		public override void Down()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
